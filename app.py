07dac4343539f6e5f5bc6b44ae0a18eaab94c6f8
sg406 <sg406@duke.edu>
2022-03-20 20:15:48 -0500

    added postgres support for heroku

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -5,10 +7,10 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
+
 
 app = Flask(__name__)
-app.config['SQL_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQL_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'wamgu'
 api = Api(app)
